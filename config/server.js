8e4d31dc16b56a736b37480edf7c340cab9be91b
Ricardo Lima <ricardo.lxique@gmail.com>
2021-09-27 18:22:04 -0500

    Adjust Middleware 405 error

--- config/server.js
+++ config/server.js
@@ -6,4 +6,10 @@ module.exports = ({ env }) => ({
       secret: env('ADMIN_JWT_SECRET', 'c131e5ec05bf4e482ee3c400ff6631f2'),
     },
   },
+  settings: {
+    "cors": {
+	    "enabled": true,
+	    "headers": "*"
+	}
+  }
 });
