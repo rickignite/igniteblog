b785a9e1d7a93efb7be95fee63c54bb733b1bbba
Ricardo Lima <ricardo.lxique@gmail.com>
2021-09-28 12:02:45 -0500

    Configuration of cloudinary

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,14 @@
-module.exports = ({ env }) => ({
-  url: env('https://morning-everglades-28972.herokuapp.com/'),
-});
+module.exports = ({ env }) => (
+	{
+  		url: env('https://morning-everglades-28972.herokuapp.com/'),
+  		upload: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: 'rickignitemedia',
+            api_key: '976551539418336',
+            api_secret: 'jfwNtaJ0aIlL1Zws2_X9HKxOl2E',
+          },
+        },
+	}
+);
  
---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: 'rickignitemedia',
+      api_key: '976551539418336',
+      api_secret: 'jfwNtaJ0aIlL1Zws2_X9HKxOl2E',
+    },
+  },    
+});
